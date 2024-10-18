831464b660be157649d0de7b4a9e4836fcc22031
Zerum <imsoug071@gmail.com>
2024-10-18 11:00:48 +0900

    added : footer 페이지 추가함.

--- src/app/(providers)/(root)/layout.tsx
+++ src/app/(providers)/(root)/layout.tsx
@@ -1,11 +1,13 @@
 import React, { PropsWithChildren } from "react";
 import Header from "./_components/header/header";
+import Footer from "./_components/footer/footer";
 
 function RootLayout({ children }: PropsWithChildren) {
   return (
     <div id="root-layout" className="bg-black">
       <Header />
       {children}
+      <Footer />
     </div>
   );
 }
