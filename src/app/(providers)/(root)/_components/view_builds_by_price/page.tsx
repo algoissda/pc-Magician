50a121e58c2a4a52c53b3ca2c81b674c6b5e1693
Zerum <imsoug071@gmail.com>
2024-10-21 16:32:29 +0900

    chore: removed meaningless codes and components

--- src/app/(providers)/(root)/_components/view_builds_by_price/page.tsx
+++ src/app/(providers)/(root)/_components/view_builds_by_price/page.tsx
@@ -1,9 +1,7 @@
-import React from 'react'
+import React from "react";
 
 function ViewBuildsByPrice() {
-  return (
-    <h3 className="text-2xl font-semibold text-white">가격에 따른 빌드 보기</h3>
-  )
+  return <main>ViewBuildsByPricePage</main>;
 }
 
-export default ViewBuildsByPrice
+export default ViewBuildsByPrice;
