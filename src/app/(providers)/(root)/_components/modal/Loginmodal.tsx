88c54e88abd512880765a5dc4cfdc1f716a56df9
Zerum <imsoug071@gmail.com>
2024-10-25 10:55:26 +0900

    added : 로그인 모달 테두리에 rgb 추가함

--- src/app/(providers)/(root)/_components/modal/Loginmodal.tsx
+++ src/app/(providers)/(root)/_components/modal/Loginmodal.tsx
@@ -12,8 +12,7 @@ interface ModalProps {
 }
 
 const Modal = ({ open, onClose, children }: ModalProps) => {
-
-  const theme = useThemeStore((state) => state.theme)
+  const theme = useThemeStore((state) => state.theme);
 
   const router = useRouter();
   const [email, setEmail] = useState("");
@@ -57,7 +56,10 @@ const Modal = ({ open, onClose, children }: ModalProps) => {
     handleClickLogInButton();
   };
 
-  
+  const lineThemeStyle =
+    theme === "dark"
+      ? ""
+      : "linear-gradient(to right, #a855f7 , #6b21a8 , #3b0764 , #000000)";
 
   return ReactDOM.createPortal(
     <>
@@ -66,63 +68,109 @@ const Modal = ({ open, onClose, children }: ModalProps) => {
         className="bg-black/50 flex items-center justify-center fixed top-0 left-0 right-0 bottom-0 z-20"
       >
         <div
-          onClick={handleClickModalBody}
-          className={`modal_body ${
-            theme === "dark" ? "bg-[#0d1117]" : "bg-white"
-          } rounded-md w-full max-w-[400px] px-5 py-8`}
+          className="border-8 custom-border-black"
+          style={{
+            background: lineThemeStyle,
+          }}
         >
-          <h2 className={`font-bold text-3xl text-center my-12 ${
-            theme === "dark" ? "text-white" : "text-[#0d1117]"
-          } `}>Log-In</h2>
-          <form onSubmit={handleSubmit} className="flex flex-col items-center gap-y-4 max-w-sm mx-auto w-full">
-            <div className="grid gap-y-1.5 w-full">
-              <label
-                htmlFor="email"
-                className={`text-sm font-medium ${
-                  theme === "dark" ? "text-white" : "text-[#0d1117]"
-                } `}
-              >
-                Email
-              </label>
-              <input
-                id="email"
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
-                type="email"
-                className={`block border w-full px-6 py-3 rounded focus:border-black outline:none transition  ${
-                  theme === "dark" ? " bg-[#0d1117] text-white" : " bg-white text-black "
-                }`}
-              />
-            </div>
-            <div className="grid gap-y-1.5 w-full">
-              <label
-                htmlFor="password"
-                className={`text-sm font-medium ${
-                  theme === "dark" ? "text-white" : "text-[#0d1117]"
-                } `}
-              >
-                Password
-              </label>
-              <input
-                id="password"
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-                type="password"
-                className= {`block border w-full px-6 py-3 rounded focus:border-black outline:none transition  ${
-                  theme === "dark" ? " bg-[#0d1117] text-white" : " bg-white text-black "
-                } `}
-              />
-            </div>
-            <button
-              type="submit"
-              className={`border-slate-700 py-4 px-12 text-[15px] w-full font-semibold ${
-                theme === "dark" ? " text-[#0d1117] bg-white" : " text-white bg-[#0d1117] "
+          <style jsx>{`
+            .custom-border-black {
+              border-width: 4px;
+              border-image: linear-gradient(
+                  to right,
+                  #0ea5e9,
+                  #3730a3,
+                  #c026d3,
+                  #e11d48
+                )
+                1;
+            }
+          `}</style>
+          {/* <style jsx>{`
+            .custom-border-white {
+              border-width: 4px;
+              border-image: linear-gradient(
+                  to right,
+                  #a855f7,
+                  #6b21a8,
+                  #3b0764,
+                  #e11d48
+                )
+                1;
+            }
+          `}</style> */}
+          <div
+            onClick={handleClickModalBody}
+            className={`modal_body ${
+              theme === "dark" ? "bg-[#0d1117]" : "bg-white"
+            } rounded-md w-full max-w-[400px] px-5 py-8`}
+          >
+            <h2
+              className={`font-bold text-3xl text-center my-12 ${
+                theme === "dark" ? "text-white" : "text-[#0d1117]"
               } `}
             >
-              Log in
-            </button>
-            {children}
-          </form>
+              Log-In
+            </h2>
+            <form
+              onSubmit={handleSubmit}
+              className="flex flex-col items-center gap-y-4 max-w-sm mx-auto w-full"
+            >
+              <div className="grid gap-y-1.5 w-full">
+                <label
+                  htmlFor="email"
+                  className={`text-sm font-medium ${
+                    theme === "dark" ? "text-white" : "text-[#0d1117]"
+                  } `}
+                >
+                  Email
+                </label>
+                <input
+                  id="email"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                  type="email"
+                  className={`block border w-full px-6 py-3 rounded focus:border-black outline:none transition  ${
+                    theme === "dark"
+                      ? " bg-[#0d1117] text-white"
+                      : " bg-white text-black "
+                  }`}
+                />
+              </div>
+              <div className="grid gap-y-1.5 w-full">
+                <label
+                  htmlFor="password"
+                  className={`text-sm font-medium ${
+                    theme === "dark" ? "text-white" : "text-[#0d1117]"
+                  } `}
+                >
+                  Password
+                </label>
+                <input
+                  id="password"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                  type="password"
+                  className={`block border w-full px-6 py-3 rounded focus:border-black outline:none transition  ${
+                    theme === "dark"
+                      ? " bg-[#0d1117] text-white"
+                      : " bg-white text-black "
+                  } `}
+                />
+              </div>
+              <button
+                type="submit"
+                className={`border-slate-700 py-4 px-12 text-[15px] w-full font-semibold ${
+                  theme === "dark"
+                    ? " text-[#0d1117] bg-white"
+                    : " text-white bg-[#0d1117] "
+                } `}
+              >
+                Log in
+              </button>
+              {children}
+            </form>
+          </div>
         </div>
       </div>
     </>,
