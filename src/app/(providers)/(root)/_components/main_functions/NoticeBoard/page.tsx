26f1e017692f8ae4b557fa3fa6a4dbf6fce2e855
sangki <sangki0706@gmail.com>
2024-10-28 15:25:10 +0900

    modify:게시글 추가 부분 모달로 변경 및 수정

--- src/app/(providers)/(root)/_components/main_functions/NoticeBoard/page.tsx
+++ src/app/(providers)/(root)/_components/main_functions/NoticeBoard/page.tsx
@@ -109,10 +109,8 @@ function NoticeBoard() {
                   {post.like}
                 </span>
                 </div>
-
                 )
               })}
-
             </section>
           </article>
         </section>
